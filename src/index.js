ad1e161f23179438350b7813203d4effa6d3d5f0
sunxuan <1406243464@qq.com>
2021-03-31 19:14:18 +0800

    add Animation

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import 'normalize.css'
 import './assets/css/index.less'
 import { Provider } from 'react-redux'
 import { store } from './store/index'
-
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
--- src/views/layout/banner/index.jsx
+++ src/views/layout/banner/index.jsx
@@ -17,45 +17,51 @@ const Banner = (props) => {
   }
 
   return (
-    <div className='banner'>
-      <div className='content'>
-        <div className='banner_content'>
-          <div>
-            <h2 className='banner_title'>
-              <FormattedMessage id='banner_text_1' />
-            </h2>
-            <p className='banner_text_top'>
-              <FormattedMessage id='banner_text_2' />
-            </p>
-            <p className='banner_text'>
-              <FormattedMessage id='banner_text_3' />
-            </p>
-            <p className='banner_text'>
-              <FormattedMessage id='banner_text_7' />
-            </p>
-          </div>
-          <div className='banner_btn'>
-            <a href='https://docsend.com/view/428qibbtd9xb732h' target='_blank'>
-              <FormattedMessage id='banner_text_4' />
-            </a>
-            {/* <a
+    <>
+      <div id='container'></div>
+      <div className='banner'>
+        <div className='content'>
+          <div className='banner_content'>
+            <div>
+              <h2 className='banner_title'>
+                <FormattedMessage id='banner_text_1' />
+              </h2>
+              <p className='banner_text_top'>
+                <FormattedMessage id='banner_text_2' />
+              </p>
+              <p className='banner_text'>
+                <FormattedMessage id='banner_text_3' />
+              </p>
+              <p className='banner_text'>
+                <FormattedMessage id='banner_text_7' />
+              </p>
+            </div>
+            <div className='banner_btn'>
+              <a
+                href='https://docsend.com/view/428qibbtd9xb732h'
+                target='_blank'
+              >
+                <FormattedMessage id='banner_text_4' />
+              </a>
+              {/* <a
               onClick={(e) => {
                 jumpPosition(e)
               }}
             >
               <FormattedMessage id='banner_text_5' />
             </a> */}
-            <a
-              onClick={(e) => {
-                // goDetail(e)
-              }}
-            >
-              <FormattedMessage id='banner_text_6' />
-            </a>
+              <a
+                onClick={(e) => {
+                  // goDetail(e)
+                }}
+              >
+                <FormattedMessage id='banner_text_6' />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
