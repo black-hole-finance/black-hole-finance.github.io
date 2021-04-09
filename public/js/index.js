c70686fb685e72e9944ed1ac30bfa7660c265b44
sunxuan <1406243464@qq.com>
2021-04-09 18:31:08 +0800

    111

--- public/js/index.js
+++ public/js/index.js
@@ -9,9 +9,9 @@ container.appendChild(renderer.domElement)
 var scene = new THREE.Scene()
 
 if (width > 747) {
-  var camera = new THREE.PerspectiveCamera(55, aspect, 0.1, 1000)
+  var camera = new THREE.PerspectiveCamera(50, aspect, 0.1, 1000)
   camera.position.z = 500
-  camera.position.set(-140, 20, 500)
+  camera.position.set(-200, 20, 500)
 } else {
   var camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1000)
   camera.position.z = 500
@@ -48,9 +48,9 @@ for (var p = 0; p < Math.PI * 2; p = p + Math.random() * 0.15) {
 
   rand = Math.random() * 60 - 30
   asteroid.position.set(
-    170 * Math.sin(p) + rand,
+    160 * Math.sin(p) + rand,
     rand,
-    170 * Math.cos(p) + rand
+    160 * Math.cos(p) + rand
   )
 
   asteroid.geometry.computeFlatVertexNormals()
@@ -59,7 +59,7 @@ for (var p = 0; p < Math.PI * 2; p = p + Math.random() * 0.15) {
 
 system.add(asteroids)
 // 围绕大球的小颗粒位置和方向
-system.rotation.x = 0.1
+system.rotation.x = 0.2
 system.rotation.y = -0.3
 system.rotation.z = -0.4
 
