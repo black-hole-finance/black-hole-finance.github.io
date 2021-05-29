96891d76f768bf9da4a60be496fcbc3f1c4e3282
sunxuan <1406243464@qq.com>
2021-05-29 23:57:07 +0800

    modify logo

--- src/locale/cn.js
+++ src/locale/cn.js
@@ -5,8 +5,7 @@ const zh_CN = {
   header_text_4: '空投',
   header_text_5: 'BLACK',
   banner_text_1: '黑洞协议',
-  banner_text_2:
-    '无许可去中心化跨链燃烧协议，永久通缩的区块链生态重构者。',
+  banner_text_2: '无许可去中心化跨链燃烧协议，永久通缩的区块链生态重构者。',
   banner_text_3:
     'The Blackhole protocol is an approval-free cross-chain burning platform based on Ethereum network. Any user or project governor could create a burning pool by holding BLACK and old token LP to permanently burn the old Token into a new Token, thus gaining a new ecological vitality.',
   banner_text_4: '项目概括（简介）',
@@ -16,10 +15,9 @@ const zh_CN = {
     'Blackhole protocol originates from Ethereum network and gradually spread to Polkadot, BSC, Heco, Sol and eventually the whole blockchain world. ',
   banner_text_8: '{coin}合约地址:',
   banner_text_9: 'Launch APP',
-  home_text_1:
-    '治理代币Black token 黑洞通缩燃烧机制示意',
+  home_text_1: '治理代币Black token 黑洞通缩燃烧机制示意',
   home_text_2:
-    "创建者持有X代币和Black的流动性即可无许可地创建黑洞燃烧池，在燃烧池中投入新代币Y激励燃烧。同时Black也会随着燃烧的进度线性返回。",
+    '创建者持有X代币和Black的流动性即可无许可地创建黑洞燃烧池，在燃烧池中投入新代币Y激励燃烧。同时Black也会随着燃烧的进度线性返回。',
   home_text_3: '路线图',
   home_text_4: '2021年Q2',
   home_text_4_2: 'V0.1 Fermion（费米子)',
@@ -48,16 +46,14 @@ const zh_CN = {
   home_text_24: '即将开始',
   home_text_25: '投资机构',
   home_text_26: '合作伙伴',
-  detail_text_1:
-    '这是一个智能合约地址，请不要将任何代币转移到其中。',
+  detail_text_1: '这是一个智能合约地址，请不要将任何代币转移到其中。',
   detail_text_2: '种子轮',
   detail_text_3: '兑换数量',
   detail_text_4: '我的额度',
   detail_text_5: '兑换进度',
   detail_text_6: '参与',
   detail_text_7: '查看',
-  detail_text_8:
-    '抱歉，您没有参与该项目，因为您没有参与白名单。',
+  detail_text_8: '抱歉，您没有参与该项目，因为您没有参与白名单。',
   detail_text_9: '详情',
   detail_text_10: '关于项目',
   detail_text_11: '投资者信息',
@@ -77,13 +73,12 @@ const zh_CN = {
   connect_wallet_text_2:
     '欢迎加入BlackHole大家庭！请连接您的钱包以查看您是否符合捐款条件',
   connect_wallet_text_3: '连接钱包',
-  failePopup_text_1:
-    '抱歉，您没有参与该项目，因为您没有参与白名单。',
+  failePopup_text_1: '抱歉，您没有参与该项目，因为您没有参与白名单。',
   successPopup_text_1: '你捐献的额度是 : ',
   successPopup_text_2: '批准',
   successPopup_text_3: '捐助',
   changeNetwork_text_1:
-    "我们检测到您需要为此Dapp将钱包的网络从本地网络切换到主网络。",
+    '我们检测到您需要为此Dapp将钱包的网络从本地网络切换到主网络。',
   changeNetwork_text_2:
     '*某些钱包可能不支持更改网络。如果您无法更改钱包中的网络，您可以考虑切换到不同的钱包。',
   changeNetwork_text_3: '切换钱包',
@@ -106,7 +101,7 @@ const zh_CN = {
   burn2:
     '黑洞协议（Blackhole Protocol）起源于Ethereum网络，逐步将向Polkadot、BSC、Heco、SOL辐射，最终辐射整个区块链世界。任何用户或者项目治理者都可以通过持有黑洞协议燃烧代币BLACK与旧Token的组合创建燃烧池从而永久的销毁旧Token并产生全新的Token，从而赋予新项目获取全新的生态活力。',
   burn3: '敬请期待',
-  burn4: 'BlackHole 正在燃烧',
+  burn4: 'SHIB 燃烧池',
   burn5: 'DAYS',
   burn6: 'HOURS',
   burn7: 'MINUTES',
@@ -118,11 +113,11 @@ const zh_CN = {
   burn13: '我的',
   burn14: '授权',
   burn15: '燃烧',
-  burn16: '添加{token}',
-  burn17: '添加{token}到',
+  burn16: '{token} 合约地址',
+  burn17: '添加 {token} 到',
   burn18: '{token}奖励',
-  burn19: '添加{token}',
-  burn20: '添加{token}到',
+  burn19: '{token} 合约地址',
+  burn20: '添加 {token} 到',
   burn21: '提取',
   activate1: 'iBLACK Activate',
   activate2: 'Swap IIO TOKEN',
--- src/views/components/burn/burnCard/index.jsx
+++ src/views/components/burn/burnCard/index.jsx
@@ -13,9 +13,7 @@ import {
   useTokenDecimals,
 } from '../../../../hooks/wallet'
 import ERC20 from '../../../../constants/abis/erc20.json'
-import {
-  getContract,
-} from '../../../../constants'
+import { getContract } from '../../../../constants'
 import { connect } from 'react-redux'
 import Timer from 'react-compound-timer'
 import OLD from '../../../../assets/image/burn/old@2x.png'
@@ -24,7 +22,14 @@ import { useBurn } from '../../../../hooks/burn'
 
 const Burn = (props) => {
   const { active, chainId, library, account } = useActiveWeb3React()
-  const { dispatch, address, stakingToken, rewardsToken, stakingTokenSymbol, rewardsTokenSymbol } = props
+  const {
+    dispatch,
+    address,
+    stakingToken,
+    rewardsToken,
+    stakingTokenSymbol,
+    rewardsTokenSymbol,
+  } = props
   const [amount, setAmount] = useState('')
   const [loadFlag, setLoadFlag] = useState(false)
   const [claimLoadFlag, setClaimLoadFlag] = useState(false)
@@ -150,7 +155,7 @@ const Burn = (props) => {
           type: 'ERC20',
           options: {
             address: rewardsToken,
-            symbol:  rewardsTokenSymbol,
+            symbol: rewardsTokenSymbol,
             decimals: 18,
             image: '',
           },
@@ -453,7 +458,10 @@ const Burn = (props) => {
             {hoverFlag === 'oldAddress' && (
               <i className='tips_content'>{stakingToken}</i>
             )}
-            <FormattedMessage id='burn16'  values={{token: stakingTokenSymbol}}/>
+            <FormattedMessage
+              id='burn16'
+              values={{ token: stakingTokenSymbol }}
+            />
             <CopyToClipboard
               text='0x0'
               onCopy={() => {
@@ -461,25 +469,28 @@ const Burn = (props) => {
               }}
             >
               <svg
-                t='1620653809614'
-                className='icon'
+                t='1622303053476'
+                class='icon'
                 viewBox='0 0 1024 1024'
                 version='1.1'
                 xmlns='http://www.w3.org/2000/svg'
-                p-id='1660'
-                width='20'
-                height='20'
+                p-id='1279'
+                width='16'
+                height='16'
               >
                 <path
-                  d='M394.666667 106.666667h448a74.666667 74.666667 0 0 1 74.666666 74.666666v448a74.666667 74.666667 0 0 1-74.666666 74.666667H394.666667a74.666667 74.666667 0 0 1-74.666667-74.666667V181.333333a74.666667 74.666667 0 0 1 74.666667-74.666666z m0 64a10.666667 10.666667 0 0 0-10.666667 10.666666v448a10.666667 10.666667 0 0 0 10.666667 10.666667h448a10.666667 10.666667 0 0 0 10.666666-10.666667V181.333333a10.666667 10.666667 0 0 0-10.666666-10.666666H394.666667z m245.333333 597.333333a32 32 0 0 1 64 0v74.666667a74.666667 74.666667 0 0 1-74.666667 74.666666H181.333333a74.666667 74.666667 0 0 1-74.666666-74.666666V394.666667a74.666667 74.666667 0 0 1 74.666666-74.666667h74.666667a32 32 0 0 1 0 64h-74.666667a10.666667 10.666667 0 0 0-10.666666 10.666667v448a10.666667 10.666667 0 0 0 10.666666 10.666666h448a10.666667 10.666667 0 0 0 10.666667-10.666666v-74.666667z'
-                  p-id='1661'
+                  d='M384 320V224a32 32 0 0 1 32-32h384a32 32 0 0 1 32 32v448a32 32 0 0 1-32 32H704v96c0 17.664-14.4 32-32.256 32H288.192A32 32 0 0 1 256 800l0.128-448c0-17.664 14.336-32 32.192-32H384z m-63.872 64L320 768h320V384H320.128zM448 320h256v320h64V256H448v64z'
+                  p-id='1280'
                 ></path>
               </svg>
             </CopyToClipboard>
           </p>
 
           <p onClick={addOldToken}>
-            <FormattedMessage id='burn17' values={{token: stakingTokenSymbol}}/>
+            <FormattedMessage
+              id='burn17'
+              values={{ token: stakingTokenSymbol }}
+            />
             <span className='metaMask_logo'></span>
           </p>
         </div>
@@ -489,7 +500,10 @@ const Burn = (props) => {
           style={{ alignItems: 'self-start' }}
         >
           <p className='new_ewards'>
-            <FormattedMessage id='burn18' values={{token: rewardsTokenSymbol}}/>
+            <FormattedMessage
+              id='burn18'
+              values={{ token: rewardsTokenSymbol }}
+            />
             <span>{(burn && formatAmount(burn.earned)) || '-'}</span>
           </p>
 
@@ -514,7 +528,10 @@ const Burn = (props) => {
             {hoverFlag === 'newAddress' && (
               <i className='tips_content'>{rewardsToken}</i>
             )}
-            <FormattedMessage id='burn19' values={{token: rewardsTokenSymbol}}/>
+            <FormattedMessage
+              id='burn19'
+              values={{ token: rewardsTokenSymbol }}
+            />
             <CopyToClipboard
               text='0x0'
               onCopy={() => {
@@ -522,25 +539,28 @@ const Burn = (props) => {
               }}
             >
               <svg
-                t='1620653809614'
-                className='icon'
+                t='1622303053476'
+                class='icon'
                 viewBox='0 0 1024 1024'
                 version='1.1'
                 xmlns='http://www.w3.org/2000/svg'
-                p-id='1660'
-                width='20'
-                height='20'
+                p-id='1279'
+                width='16'
+                height='16'
               >
                 <path
-                  d='M394.666667 106.666667h448a74.666667 74.666667 0 0 1 74.666666 74.666666v448a74.666667 74.666667 0 0 1-74.666666 74.666667H394.666667a74.666667 74.666667 0 0 1-74.666667-74.666667V181.333333a74.666667 74.666667 0 0 1 74.666667-74.666666z m0 64a10.666667 10.666667 0 0 0-10.666667 10.666666v448a10.666667 10.666667 0 0 0 10.666667 10.666667h448a10.666667 10.666667 0 0 0 10.666666-10.666667V181.333333a10.666667 10.666667 0 0 0-10.666666-10.666666H394.666667z m245.333333 597.333333a32 32 0 0 1 64 0v74.666667a74.666667 74.666667 0 0 1-74.666667 74.666666H181.333333a74.666667 74.666667 0 0 1-74.666666-74.666666V394.666667a74.666667 74.666667 0 0 1 74.666666-74.666667h74.666667a32 32 0 0 1 0 64h-74.666667a10.666667 10.666667 0 0 0-10.666666 10.666667v448a10.666667 10.666667 0 0 0 10.666666 10.666666h448a10.666667 10.666667 0 0 0 10.666667-10.666666v-74.666667z'
-                  p-id='1661'
+                  d='M384 320V224a32 32 0 0 1 32-32h384a32 32 0 0 1 32 32v448a32 32 0 0 1-32 32H704v96c0 17.664-14.4 32-32.256 32H288.192A32 32 0 0 1 256 800l0.128-448c0-17.664 14.336-32 32.192-32H384z m-63.872 64L320 768h320V384H320.128zM448 320h256v320h64V256H448v64z'
+                  p-id='1280'
                 ></path>
               </svg>
             </CopyToClipboard>
           </p>
 
           <p onClick={addNewToken}>
-            <FormattedMessage id='burn20' values={{token: rewardsTokenSymbol}} />
+            <FormattedMessage
+              id='burn20'
+              values={{ token: rewardsTokenSymbol }}
+            />
             <span className='metaMask_logo'></span>
           </p>
         </div>
